b53c1072705b4d2b710c19165f06207eca6ac72a
Tudor Vari <tudor.vari@gmail.com>
2022-12-02 16:40:57 +0100

    day1 done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AdventOfCode2022
-Solutions for the Advent of Code 2022
+Solutions for the Advent of Code 2022 written in C++
--- advent_day1/advent_day1.cpp
+++ advent_day1/advent_day1.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,16 +11,17 @@ int main()
 	cout << "Welcome to Advent of Code 2022 - Day 1\n";
 
 	ifstream input("input.txt");
-	int item = 0, sum = 0, lineCounter = 0, i = 0;
+	int sum = 0, lineCounter = 0, maxItem = 0, maxItemPosition = 0, i = 0;
 	string line;
 	vector<string> readData;
 
+	//read data from txt file and manipulate it
 	if (input.is_open())
 	{
-		cout << "\nReading file...\n\n";
+		cout << "\nReading file...\n";
 		while (getline(input, line))
 		{
-			cout << line << endl;
+			//cout << line << endl;
 			if (line == "")
 				readData.push_back("$");
 			else
@@ -29,24 +30,53 @@ int main()
 		}
 		input.close();
 		cout << "\nDone!\n";
-		cout << "There are " << lineCounter << " new lines in the file.\n";
+		cout << "\nThere are " << lineCounter << " new lines in the file.\n";
 	}
 	else
 		cout << "Reading the file failed!";
 
-	vector<int> sumArr(lineCounter, 0);
+	//calculate sum
+	vector<int> sumArr;
+	//cout << "\nreadData: \n";
+	for (int k = 0; k < lineCounter; k++)
+	{
+		//cout << readData[k] << endl;
+		if (readData[k] != "$")
+		{
+			sum += stoi(readData[k]);
+		}
+		else
+		{
+			sumArr.push_back(sum);
+			sum = 0;
+			i++;
+			continue;
+		}
+	}
 
-	cout << "\nArray: \n";
-	for (int k = 0; k < lineCounter; k++)
+	//calculate biggest sum of sums and their position in array
+	//cout << "\nsumArr:\n";
+	for (int k = 0; k < i; k++)
 	{
-		sumArr[k] = k;
-		cout << sumArr[k] << endl;
+		//cout << sumArr[k] << endl;
+		if (sumArr[k] > maxItem)
+		{
+			maxItem = sumArr[k];
+			maxItemPosition = k;
+		}
 	}
-	cout << "\nOriginal vector Data: \n";
-	for (int k = 0; k < lineCounter; k++)
-	{
-		cout << readData[k] << endl;
-	}
+	cout << endl << "#" << maxItemPosition + 1 << " Elf is carrying the most calories: " << maxItem << endl;
+
+	//sort array and take top 3 biggest sums
+	sort(sumArr.begin(), sumArr.end());
+	//cout << "\nsumArr sorted:\n";
+	//for (auto x : sumArr)
+	//{
+	//	cout << x << endl;
+	//}
+	int topThree = sumArr[i - 1] + sumArr[i - 2] + sumArr[i - 3];
+	cout << "\nTotal calories of top 3 Elves is: " << topThree << endl;
+
 	return 0;
 }
 
